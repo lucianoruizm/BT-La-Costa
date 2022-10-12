617fb8097513cefc0b8dcdaf3479d644f0a0ec49
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-12 17:29:15 -0300

    login con loclastorage

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,78 @@
 document.querySelector('.form-container').addEventListener('submit', (e) => {
     e.preventDefault();
 })
+
+function clear(){
+    const inputs = document.querySelectorAll('.input-login');
+    // Clear the content of each input
+    inputs.forEach((input) => input.value = '');
+}
+
+// Login
+function store(){
+    let email = document.getElementById('email');
+    let pw = document.getElementById('pw');
+    let repeatPw = document.getElementById('repeatPw');
+    let lowerCaseLetters = /[a-z]/g;
+    let upperCaseLetters = /[A-Z]/g;
+    let numbers = /[0-9]/g;
+
+    if(email.value.length == 0){
+        alert('Por favor completar en email');
+
+    }else if(pw.value.length == 0){
+        alert('Por favor completar en password');
+
+    }else if(email.value.length == 0 && pw.value.length == 0){
+        alert('Por favor completar en email y password');
+
+    }else if(pw.value.length > 8){
+        alert('Máximo 8 caracteres');
+
+    }else if(!pw.value.match(numbers)){
+        alert('Por favor agregue al menos un número');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('Por favor agregue al menos una letra mayúscula');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('Por favor agregue al menos una letra minúscula');
+
+    }else if(pw.value != repeatPw.value){
+        alert('Las contraseñas no coinciden');
+
+    }else{
+        localStorage.setItem('email', email.value);
+        localStorage.setItem('pw', pw.value);
+        clear()
+        alert('Tu cuenta ha sido creada');
+    }
+}
+
+// Checking login
+function check(){
+
+    let storedEmail = localStorage.getItem('email');
+    let storedPw = localStorage.getItem('pw');
+    let userEmail = document.getElementById('userEmail');
+    let userPw = document.getElementById('userPw');
+    
+    
+    if(userEmail.value == storedEmail && userPw.value == storedPw){
+        localStorage.setItem('loginChecked', 'isChecked');
+        alert('Inicio de sesión exitoso');
+        window.open('index.html','_self');
+    }else{
+        localStorage.removeItem('loginChecked');
+        alert('Error al iniciar sesión');
+    }
+
+    clear()
+}
+
+
+if(!localStorage.getItem('loginChecked')){
+      console.log('is not checked')
+}else{
+    console.log('is checked')
+}
--- scripts/script.js
+++ scripts/script.js
@@ -8,77 +8,14 @@ hamburger.addEventListener("click", function() {
     menu.classList.toggle('show')
 });
 
-function clear(){
-    const inputs = document.querySelectorAll('.input-login');
-    // Clear the content of each input
-    inputs.forEach((input) => input.value = '');
-}
-
-// Login
-
-
-
-
-function store(){
-    let email = document.getElementById('email');
-    let pw = document.getElementById('pw');
-    let repeatPw = document.getElementById('repeatPw');
-    let lowerCaseLetters = /[a-z]/g;
-    let upperCaseLetters = /[A-Z]/g;
-    let numbers = /[0-9]/g;
-
-    if(email.value.length == 0){
-        alert('Por favor completar en email');
-
-    }else if(pw.value.length == 0){
-        alert('Por favor completar en password');
-
-    }else if(email.value.length == 0 && pw.value.length == 0){
-        alert('Por favor completar en email y password');
-
-    }else if(pw.value.length > 8){
-        alert('Máximo 8 caracteres');
-
-    }else if(!pw.value.match(numbers)){
-        alert('Por favor agregue al menos un número');
-
-    }else if(!pw.value.match(upperCaseLetters)){
-        alert('Por favor agregue al menos una letra mayúscula');
-
-    }else if(!pw.value.match(lowerCaseLetters)){
-        alert('Por favor agregue al menos una letra minúscula');
-
-    }else if(pw.value != repeatPw.value){
-        alert('Las contraseñas no coinciden');
-
-    }else{
-        localStorage.setItem('email', email.value);
-        localStorage.setItem('pw', pw.value);
-        clear()
-        alert('Tu cuenta ha sido creada');
-    }
-}
-
-// Checking login
-function check(){
-
-    let storedEmail = localStorage.getItem('email');
-    let storedPw = localStorage.getItem('pw');
-    let userEmail = document.getElementById('userEmail');
-    let userPw = document.getElementById('userPw');
-    let loginBtn = document.querySelector('.login');
-    let loginChecked = false;
+if(!localStorage.getItem('loginChecked')){
+    console.log('is not checked')
+}else{
+    console.log('is checked')
+    let loginContent = document.querySelector('.login');
+    loginContent.textContent = 'Cerrar Sesión';
     
-    if(userEmail.value == storedEmail && userPw.value == storedPw){
-        loginChecked = true;
-        alert('Inicio de sesión exitoso');
-    }else{
-        alert('Error al iniciar sesión');
-    }
-    
-    clear()
-    console.log(loginChecked)
+    loginContent.addEventListener('click', (e) => {
+        localStorage.removeItem('loginChecked');
+    })
 }
-
-
-
