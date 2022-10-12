f3db4fbb69677490c88827766590dde5e37b09fc
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-12 13:44:11 -0300

    login localstorage

---
+++ scripts/login.js
@@ -0,0 +1,3 @@
+document.querySelector('#login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+})
--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,63 @@ hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     menu.classList.toggle('show')
 });
+
+
+// Login
+function store(){
+    let email = document.getElementById('email');
+    let pw = document.getElementById('pw');
+    let repeatPw = document.getElementById('repeatPw');
+    let lowerCaseLetters = /[a-z]/g;
+    let upperCaseLetters = /[A-Z]/g;
+    let numbers = /[0-9]/g;
+
+    if(email.value.length == 0){
+        alert('Por favor completar en email');
+
+    }else if(pw.value.length == 0){
+        alert('Por favor completar en password');
+
+    }else if(email.value.length == 0 && pw.value.length == 0){
+        alert('Por favor completar en email y password');
+
+    }else if(pw.value.length > 8){
+        alert('Máximo 8 caracteres');
+
+    }else if(!pw.value.match(numbers)){
+        alert('Por favor agregue al menos un número');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('Por favor agregue al menos una letra mayúscula');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('Por favor agregue al menos una letra minúscula');
+
+    }else if(pw.value != repeatPw.value){
+        alert('Las contraseñas no coinciden');
+
+    }else{
+        localStorage.setItem('email', email.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Tu cuenta ha sido creada');
+    }
+}
+
+// Checking login
+function check(){
+
+    let storedEmail = localStorage.getItem('email');
+    let storedPw = localStorage.getItem('pw');
+    let userEmail = document.getElementById('userEmail');
+    let userPw = document.getElementById('userPw');
+    let loginChecked = false;
+    
+    if(userEmail.value == storedEmail && userPw.value == storedPw){
+        loginChecked = true;
+    }else{
+        alert('Error al iniciar sesión');
+    }
+
+    console.log(loginChecked)
+
+}
